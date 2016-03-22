e7c87ce787173c3bdf437a64c6c28f428489c641
Michał Rygielski <Micha Rygielski>
2016-03-22 10:28:17 +0100

    update

---
+++ fuel/app/logs/2016/03/22.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2016-03-22 10:20:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
